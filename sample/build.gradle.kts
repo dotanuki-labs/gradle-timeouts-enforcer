93ec57e799ac55e452bcfb9e59ee1f3b4e86d567
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2020-05-22 23:23:39 +0200

    Bumped Gradle on sample

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -1,17 +1,46 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+buildscript {
+    repositories {
+        mavenCentral()
+        mavenLocal()
+        maven("https://plugins.gradle.org/m2/")
+    }
+
+    dependencies {
+        classpath("io.labs.dotanuki:timeouts-enforcer-plugin:+")
+    }
+}
+
 plugins {
-    id(BuildPlugins.kotlinJVM)
+    id("org.jetbrains.kotlin.jvm") version "1.3.71"
     application
 }
 
+apply(plugin = "io.labs.dotanuki.timeoutsenforcer")
+
+repositories {
+    mavenCentral()
+}
+
 dependencies {
-    implementation(Libraries.kotlinStdLib)
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.71")
 }
 
 tasks {
     withType<KotlinCompile>().configureEach {
-        kotlinOptions.jvmTarget = Definitions.targetJDK
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+    register("eureka") {
+
+        doFirst {
+            Thread.sleep(10000)
+        }
+
+        doLast {
+            println("Eureka!")
+        }
     }
 }
 
