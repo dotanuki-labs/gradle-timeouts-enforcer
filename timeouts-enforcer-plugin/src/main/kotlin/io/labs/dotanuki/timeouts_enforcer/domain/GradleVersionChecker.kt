d7f423212c4c4fa702c32e6e96bc53d9b011b5c9
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2020-05-26 03:39:13 +0200

    Added unit tests for GradleVersionChecker

--- timeouts-enforcer-plugin/src/main/kotlin/io/labs/dotanuki/timeouts_enforcer/domain/GradleVersionChecker.kt
+++ timeouts-enforcer-plugin/src/main/kotlin/io/labs/dotanuki/timeouts_enforcer/domain/GradleVersionChecker.kt
@@ -1,14 +1,22 @@
 package io.labs.dotanuki.timeouts_enforcer.domain
 
 import io.labs.dotanuki.timeouts_enforcer.domain.TimeoutEnforcerException.InvalidGradleVersion
+import io.labs.dotanuki.timeouts_enforcer.domain.TimeoutEnforcerException.UnsupportedGradleVersion
 
 internal object GradleVersionChecker {
 
     fun requireValidVersion(gradleVersion: String) {
         gradleVersion.split(".").firstOrNull()
-            ?.let { if (it.toInt() < MINIMUM_MAJOR_VERSION) throw InvalidGradleVersion(gradleVersion) }
-            ?: throw TimeoutEnforcerException.UnsupportedGradleVersion(gradleVersion)
+            ?.let { if (unsupported(it, gradleVersion)) throw UnsupportedGradleVersion(gradleVersion) }
+            ?: throw InvalidGradleVersion(gradleVersion)
     }
+
+    private fun unsupported(major: String, gradleVersion: String): Boolean =
+        try {
+            major.toInt() < MINIMUM_MAJOR_VERSION
+        } catch (error: Throwable) {
+            throw InvalidGradleVersion(gradleVersion)
+        }
 
     private const val MINIMUM_MAJOR_VERSION = 5
 }
---
+++ timeouts-enforcer-plugin/src/test/kotlin/io/labs/dotanuki/timeouts_enforcer/tests/domain/GradleVersionCheckerTests.kt
@@ -0,0 +1,29 @@
+package io.labs.dotanuki.timeouts_enforcer.tests.domain
+
+import io.labs.dotanuki.timeouts_enforcer.domain.GradleVersionChecker
+import io.labs.dotanuki.timeouts_enforcer.domain.TimeoutEnforcerException.InvalidGradleVersion
+import io.labs.dotanuki.timeouts_enforcer.domain.TimeoutEnforcerException.UnsupportedGradleVersion
+import org.assertj.core.api.Assertions.assertThatCode
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.Test
+
+class GradleVersionCheckerTests {
+
+    @Test fun `should catch invalid gradle versions`() {
+
+        val table = mapOf(
+            "3.4.1" to UnsupportedGradleVersion("3.4.1"),
+            "alpha.0.0.1" to InvalidGradleVersion("alpha.0.0.1")
+        )
+
+        table.forEach { (version, expectedError) ->
+            val execution = { GradleVersionChecker.requireValidVersion(version) }
+            assertThatThrownBy(execution).isEqualTo(expectedError)
+        }
+    }
+
+    @Test fun `should bypass on valid gradle version`() {
+        val execution = { GradleVersionChecker.requireValidVersion("6.4.1") }
+        assertThatCode(execution).doesNotThrowAnyException()
+    }
+}
